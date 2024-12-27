667a33bfcc779fce8108be6b1bbb218de18e361d
DQuinn97 <quinten2407@gmail.com>
2024-12-27 13:22:50 +0100

    Homepage MVP

--- index.php
+++ index.php
@@ -14,9 +14,65 @@ include_once "includes/css_js.inc.php";
 </head>
 
 <body>
-    <?= "php works on front website" ?>
-    <p class="icon-pacman"></p>
-    <img src="images/sample.jpg" alt="">
+    <header><img src="" alt="logo">
+        <nav>
+            <ul>
+                <li><a href="#">New Releases</a></li>
+                <li><a href="#">Categories</a></li>
+                <li><a href="#">My Lists</a></li>
+            </ul>
+            <div id="notification"></div>
+            <div id="profile"></div>
+        </nav>
+    </header>
+    <main>
+        <section id="game_highlight"></section>
+        <section id="game_list">
+            <section>
+                <h2>Filters</h2>
+                <p>Clear all</p>
+                <select name="sort" id="sort">
+                    <option value="rating_desc">highest rating</option>
+                    <option value="rating_asc">lowest rating</option>
+                    <option value="release_desc">newest</option>
+                    <option value="release_asc">oldest</option>
+                    <option value="name_asc">A-Z</option>
+                    <option value="name_desc">Z-A</option>
+                </select>
+            </section>
+            <section>
+                <section id="filter">
+                    <div>
+                        <h2>Category</h2>
+                        <p>*fill using db*</p>
+                    </div>
+                    <div>
+                        <h2>Platform</h2>
+                        <p>*fill using db*</p>
+                    </div>
+                </section>
+                <section id="content">
+                    <section class="game_card"><img src="" alt=""></section>
+                </section>
+            </section>
+        </section>
+    </main>
+    <footer>
+        <section>
+            <p>SavePoint Gaming Database</p>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </section>
+        <section>
+            <p>Stay in touch! Subscribe to our Newsletter!</p>
+            <form action=""><input type="text" name="sub_email" id="sub_email"><input type="submit" value="sub_submit" id="sub_submit"></form>
+        </section>
+    </footer>
 </body>
 
 </html>
