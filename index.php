c7a1a4df60958787850a7906539f032ac7deabfd
PpieYa <emely@ollevier.com>
2025-01-04 19:08:16 +0100

    index page grid changes grid
    changes grid and filters

--- index.php
+++ index.php
@@ -25,73 +25,71 @@ include_once "includes/css_js.inc.php";
             </div>
         </section>
         <!-- Filters -->
+        <select name="sort" id="sort">
+            <option value="rating_desc">highest rating</option>
+            <option value="rating_asc">lowest rating</option>
+            <option value="release_desc">newest</option>
+            <option value="release_asc">oldest</option>
+            <option value="name_asc">A-Z</option>
+            <option value="name_desc">Z-A</option>
+        </select>
         <section id="game_section">
 
             <aside id="filters">
-                <section>
-                    <h2>Filters</h2>
-                    <p>Clear all</p>
-                    <select name="sort" id="sort">
-                        <option value="rating_desc">highest rating</option>
-                        <option value="rating_asc">lowest rating</option>
-                        <option value="release_desc">newest</option>
-                        <option value="release_asc">oldest</option>
-                        <option value="name_asc">A-Z</option>
-                        <option value="name_desc">Z-A</option>
-                    </select>
+                <h2>Filters</h2>
+                <p>Clear all</p>
+                <section id="filter">
+                    <div>
+                        <h2>Category</h2>
+                        <p>*fill using db*</p>
+                    </div>
+                    <div>
+                        <h2>Platform</h2>
+                        <p>*fill using db*</p>
+                    </div>
                 </section>
-                <section>
-                    <section id="filter">
-                        <div>
-                            <h2>Category</h2>
-                            <p>*fill using db*</p>
-                        </div>
-                        <div>
-                            <h2>Platform</h2>
-                            <p>*fill using db*</p>
-                        </div>
-                    </section>
-                    <section id="content">
-                        <section class="game_card"><img src="" alt=""></section>
-                    </section>
+                <section id="content">
+                    <section class="game_card"><img src="" alt=""></section>
+                </section>
+
+            </aside>
+
+
+            <!-- Game Grid Section -->
+            <div id="games">
+                <section id="game_list">
+                    <!-- DIT AANPASSEN NAAR PHP DB data -->
+                    <div class="game_card">
+                        <img src="/images/Chayka-Mario.webp" alt="Game 1">
+                        <div class="card_title">Game Title 1</div>
+                    </div>
+                    <div class="game_card">
+                        <img src="/images/Chayka-Mario.webp" alt="Game 1">
+                        <div class="card_title">Game Title 1</div>
+                    </div>
+                    <div class="game_card">
+                        <img src="/images/Chayka-Mario.webp" alt="Game 1">
+                        <div class="card_title">Game Title 1</div>
+                    </div>
+                    <div class="game_card">
+                        <img src="/images/Chayka-Mario.webp" alt="Game 1">
+                        <div class="card_title">Game Title 1</div>
+                    </div>
+                    <div class="game_card">
+                        <img src="/images/Chayka-Mario.webp" alt="Game 1">
+                        <div class="card_title">Game Title 1</div>
+                    </div>
+                    <div class="game_card">
+                        <img src="/images/Chayka-Mario.webp" alt="Game 1">
+                        <div class="card_title">Game Title 1</div>
+                    </div>
+                    <div class="game_card">
+                        <img src="/images/Chayka-Mario.webp" alt="Game 1">
+                        <div class="card_title">Game Title 1</div>
+                    </div>
                 </section>
         </section>
-        </section>
-
-
-        <!-- Game Grid Section -->
-        <section id="game_list">
-            <!-- DIT AANPASSEN NAAR PHP DB data -->
-            <div class="game_card">
-                <img src="/images/Chayka-Mario.webp" alt="Game 1">
-                <div class="card_title">Game Title 1</div>
-            </div>
-            <div class="game_card">
-                <img src="/images/Chayka-Mario.webp" alt="Game 1">
-                <div class="card_title">Game Title 1</div>
-            </div>
-            <div class="game_card">
-                <img src="/images/Chayka-Mario.webp" alt="Game 1">
-                <div class="card_title">Game Title 1</div>
-            </div>
-            <div class="game_card">
-                <img src="/images/Chayka-Mario.webp" alt="Game 1">
-                <div class="card_title">Game Title 1</div>
-            </div>
-            <div class="game_card">
-                <img src="/images/Chayka-Mario.webp" alt="Game 1">
-                <div class="card_title">Game Title 1</div>
-            </div>
-            <div class="game_card">
-                <img src="/images/Chayka-Mario.webp" alt="Game 1">
-                <div class="card_title">Game Title 1</div>
-            </div>
-            <div class="game_card">
-                <img src="/images/Chayka-Mario.webp" alt="Game 1">
-                <div class="card_title">Game Title 1</div>
-            </div>
-        </section>
-
+        </div>
 
     </main>
     <?php include("includes/footer.inc.php"); ?>
