83b6f5d4e822e850e1c6a96ea7ab8163306f66e0
DQuinn97 <quinten2407@gmail.com>
2025-01-11 17:04:28 +0100

    navigation login dependency

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -9,13 +9,31 @@
         <label for="menu-toggle" class="menu-icon-containter">
             <div id="menu-icon"></div>
         </label>
-        <ul>
+        <ul id="navigation">
             <li><a href="#">New Releases</a></li>
             <li><a href="#">Categories</a></li>
-            <li><a href="mylists.php">My Lists</a></li>
+            <?php if (isLoggedIn()): ?>
+                <li><a href="user_mylists.php">My Lists</a></li>
+            <?php endif; ?>
         </ul>
         <!-- <div id="notification"></div> -->
-        <div id="profile"></div>
+        <div id="profile">
+            <section>
+                <ul>
+                    <?php if (isLoggedIn()):
+                        if (isAdmin()): ?>
+                            <li><a href="admin/">admin panel</a></li>
+                        <?php endif; ?>
+                        <li><a href="user_profile.php">profile</a></li>
+                        <li><a href="user_mylists.php">my lists</a></li>
+                        <li><a href="user_logout.php">log out</a></li>
+                    <?php else: ?>
+                        <li><a href="user_login.php">log in</a></li>
+                        <li><a href="user_register.php">register</a></li>
+                    <?php endif; ?>
+                </ul>
+            </section>
+        </div>
 
     </nav>
 
--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
 import "../css/style.css";
 
-console.log("javascript works.......");
+document.getElementById("profile").onclick = (e) => {
+  let target = e.target.querySelector("section");
+  target.style.display = target.style.display == "none" ? "block" : "none";
+
+  let nav = document.getElementById("menu-toggle");
+  if (nav.checked) nav.checked = false;
+};
+window.addEventListener("click", function (e) {
+  if (!document.getElementById("profile").contains(e.target)) {
+    document.getElementById("profile").querySelector("section").style.display =
+      "none";
+  }
+});
